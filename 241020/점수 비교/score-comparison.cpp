a096ae23392d0de66d4fa7bc88e25147d57010f2
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 16:11:01 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241020/점수 비교/score-comparison.cpp
+++ 241020/점수 비교/score-comparison.cpp
@@ -7,12 +7,12 @@ int main() {
     int b_m, b_e;
     cin >> a_m >> a_e >> b_m >> b_e;
 
-    if(a_m >= b_m && a_e >= b_e){
+    if(a_m > b_m && a_e > b_e){
         cout << 1;
     }
     else {
         cout << 0;
     }
-    
+
     return 0;
 }
