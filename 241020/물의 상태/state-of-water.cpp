afde32e6935689404d8577fdd85440e2bb2e3b55
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 15:39:41 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/물의 상태/state-of-water.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    if(n < 0){
+        cout << "ice";
+    }
+    else if(n >=100){
+        cout << "vapor";
+    }
+    else{
+        cout << "water";
+    }
+
+    return 0;
+}
