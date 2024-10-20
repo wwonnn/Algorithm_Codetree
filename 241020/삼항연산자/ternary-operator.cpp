387a8c1f962c492bbcecc1b6b3e9b2668f68c732
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 15:36:25 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int s;
+    cin >> s;
+
+    s == 100 ? cout << "pass" : cout << "failure";
+    
+    return 0;
+}
