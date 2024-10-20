0013c781b0a1d0efd97fcf1995fa48daacd02787
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 15:35:02 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, max;
+    cin >> a >> b;
+
+    max = a >= b ? a : b;
+    cout << max;
+    
+    return 0;
+}
