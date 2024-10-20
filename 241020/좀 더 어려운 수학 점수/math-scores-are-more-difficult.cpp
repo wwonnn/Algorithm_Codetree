bade23d7c99761e8c0de07fb6a9b25efb0f53ae2
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 16:36:35 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a_m, a_e;
+    int b_m, b_e;
+    cin >> a_m >> a_e >> b_m >> b_e;
+
+    if(a_m > b_m){
+        cout << 'A';
+    }
+    else if(a_m < b_m){
+        cout << 'B';
+    }
+    else{
+        if(a_e > b_e){
+            cout << 'A';
+        }
+        else{
+            cout << 'B';
+        }
+    }
+    
+    return 0;
+}
