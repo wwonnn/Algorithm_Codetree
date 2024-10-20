a483933bf7367660b6db3b7a0f5f1e2cab0e4d83
wwonnn <108168611+wwonnn@users.noreply.github.com>
2024-10-20 16:47:29 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241020/윤년인가/is-leap-year.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int y;
+    cin >> y;
+
+    if(y%4 == 0){
+        if(y%100 == 0 && y%400 != 0){
+            cout << "false";
+        }
+        else{
+            cout << "true";
+        }
+    }
+    else {
+        cout << "false";
+    }
+
+    return 0;
+}
